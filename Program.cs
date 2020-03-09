2543dba683dba4bd61541f5e8e1ed2cb0949e9c8
jamesnitz <jamesanitz2@gmail.com>
2020-03-09 10:46:48 -0500

    final commit student exercises 2 complete

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentExercise
 {
@@ -15,6 +16,7 @@ namespace StudentExercise
       Exercise holidayroad = new Exercise("holiday road", "javascript");
       Exercise planner = new Exercise("urban planner", "C#");
       Exercise fitted = new Exercise("fitted", "html");
+      Exercise pushups = new Exercise("pushups", "athletics");
 
       Cohort cohort36 = new Cohort("Cohort 36");
       Cohort cohort37 = new Cohort("Cohort 37");
@@ -24,11 +26,29 @@ namespace StudentExercise
       Student audrey = new Student("Audrey", "Borgra", "audbor", cohort37);
       Student kevin = new Student("Kevin", "Kevinson", "kev.dev", cohort37);
       Student james = new Student("James", "Nitz", "nitzle", cohort37);
+      Student cooper = new Student("Cooper", "Cooperson", "coop", cohort38);
+      Student slacker = new Student("slacker", "Slackerson", "slack", cohort38);
+      Student john = new Student("john", "johnson", "jj", cohort36);
 
       Instructor rose = new Instructor("rose", "roseington", "roseallday", cohort37, "fashion");
       Instructor chortle = new Instructor("Steve", "Brownlee", "chortlehoort", cohort37, "dad jokes");
       Instructor mo = new Instructor("mo", "money", "momo", cohort37, "making cheesecake");
+      Instructor andy = new Instructor("andy", "anderson", "slackhandle1", cohort38, "coding");
+      List<Instructor> instructorList = new List<Instructor>(){
+        rose,
+        chortle,
+        mo,
+        andy
+      };
+      List<Cohort> cohortList = new List<Cohort>(){
+        cohort37,
+        cohort36,
+        cohort37
+      };
 
+
+
+      cohort36.addStudent(john);
       cohort37.addStudent(willy);
       cohort37.addStudent(audrey);
       cohort37.addStudent(kevin);
@@ -36,6 +56,7 @@ namespace StudentExercise
       cohort37.addInstructor(rose);
       cohort37.addInstructor(chortle);
       cohort37.addInstructor(mo);
+      cohort38.addInstructor(andy);
 
       chortle.assignExercise(james, planner);
       chortle.assignExercise(james, nutshell);
@@ -43,6 +64,7 @@ namespace StudentExercise
       rose.assignExercise(kevin, nutshell);
       mo.assignExercise(willy, holidayroad);
       mo.assignExercise(willy, nutshell);
+      mo.assignExercise(willy, pushups);
       rose.assignExercise(audrey, planner);
       rose.assignExercise(audrey, fitted);
 
@@ -50,6 +72,9 @@ namespace StudentExercise
       studentsList.Add(audrey);
       studentsList.Add(james);
       studentsList.Add(kevin);
+      studentsList.Add(cooper);
+      studentsList.Add(slacker);
+      studentsList.Add(john);
 
       exercisesList.Add(nutshell);
       exercisesList.Add(holidayroad);
@@ -71,6 +96,63 @@ namespace StudentExercise
           }
         }
       }
+
+      //List JAVASCRIPT EXERCISES
+      var javascriptExercises = exercisesList.Where(exercise =>
+      {
+        return exercise.Language == "javascript";
+      }).ToList();
+      foreach (var exercise in javascriptExercises)
+      {
+        Console.WriteLine($"{exercise.Name} is written in JavaScript");
+      }
+      //List students in a particular cohort EXERCISES
+      var cohort38Students = studentsList.Where(student =>
+      {
+        return student.Cohort == cohort38;
+      }).ToList();
+      foreach (var student in cohort38Students)
+      {
+        Console.WriteLine($"{student.FirstName} is in Cohort 38!");
+      }
+      //List instructors in a particular cohort EXERCISES
+      var instructorsInCohort38 = instructorList.Where(instructor =>
+      {
+        return instructor.Cohort == cohort38;
+      }).ToList();
+      foreach (var instructor in instructorsInCohort38)
+      {
+        Console.WriteLine($"{instructor.FirstName} is in Cohort 38!");
+      }
+      //Sort students by last name
+      var studentsSortedByLastName = studentsList.OrderBy(student => student.LastName).ToList();
+      foreach (var student in studentsSortedByLastName)
+      {
+        Console.WriteLine($"{student.FirstName} {student.LastName}");
+      }
+      //students not working on anything
+      var studentsNotWorking = studentsList.Where(student =>
+      {
+        return student.Exercises == null || student.Exercises.Count() == 0;
+      });
+      foreach (var student in studentsNotWorking)
+      {
+        Console.WriteLine($"{student.FirstName} {student.LastName} isnt working on anything");
+      }
+      //Sort students by who's doing the most exercise
+      var overAchiever = studentsList.OrderByDescending(student => student.Exercises.Count()).ToList().FirstOrDefault();
+      Console.WriteLine($"{overAchiever.FirstName} {overAchiever.LastName} has the most exercises ");
+
+      var groups = studentsList.GroupBy(student => student.Cohort.Name);
+
+      foreach (var group in groups)
+      {
+        Console.WriteLine($"There are {group.Count()} in {group.Key}");
+
+      }
+
+
     }
+
   }
 }
